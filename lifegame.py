92569025667fa538c5d617e5f6aa7cba65bf0f65
Yulay Gafarov <yulay.gafarov@gmail.com>
2019-04-12 23:31:45 +0500

    a lot of improvements

---
+++ lifegame.py
@@ -0,0 +1,77 @@
+'''
+Created on Apr 12, 2019
+
+@author: yulay
+'''
+import random
+
+
+class Lifegame:
+
+    def __init__(self, nrows=90, ncols=90):
+        self.ncols = ncols
+        self.nrows = nrows
+        self.ncells = 0
+        self.generation = 0
+        self.field = self._get_new_field()
+        self.randomize_field(0.1)
+    
+    def randomize_field(self, cell_alive_probability):
+        for icol in range(self.ncols):
+            for irow in range(self.nrows):
+                if random.random() < cell_alive_probability:
+                    self.field[icol][irow] = 1 
+                    self.ncells += 1
+                else :
+                    self.field[icol][irow] = 0
+    
+    def calculate_next_generation(self):
+        next_generation_field = self._get_new_field();
+        for icol in range(self.ncols):
+            for irow in range(self.nrows):
+                next_generation_field[icol][irow] = self._will_cell_live_in_next_generation(icol, irow)
+                if self.field == 0 and next_generation_field[icol][irow] == 1:
+                    self.ncells += 1
+                if self.field == 1 and next_generation_field[icol][irow] == 0:
+                    self.ncells -= 1
+        self.field = next_generation_field        
+        self.generation += 1
+        
+    def _get_new_field(self):
+        new_field = [0] * self.ncols
+        for icol in range(self.ncols):
+            new_field[icol] = [0] * self.nrows
+        return new_field
+    
+    def _will_cell_live_in_next_generation(self, icol, irow):
+        nneighbours = self._count_neighbours(icol, irow)
+        if self.field[icol][irow] == 0:
+            if nneighbours == 3:
+                return True
+            else:
+                return False
+        if self.field[icol][irow] == 1:
+            if nneighbours < 2 or nneighbours > 3:
+                return False
+            else:
+                return True
+            
+    def _count_neighbours(self, col, row):
+        nneighbours = 0
+        if self.field[col - 1][row - 1] == 1:
+            nneighbours += 1
+        if self.field[col][row - 1] == 1:
+            nneighbours += 1
+        if self.field[(col + 1) % (self.ncols - 1)][row - 1] == 1:
+            nneighbours += 1
+        if self.field[col - 1][row] == 1:
+            nneighbours += 1
+        if self.field[(col + 1) % (self.ncols - 1)][row] == 1:
+            nneighbours += 1
+        if self.field[col - 1][(row + 1) % (self.nrows - 1)] == 1:
+            nneighbours += 1
+        if self.field[col ][(row + 1) % (self.nrows - 1)] == 1:
+            nneighbours += 1
+        if self.field[(col + 1) % (self.ncols - 1)][(row + 1) % (self.nrows - 1)] == 1:
+            nneighbours += 1
+        return nneighbours
--- main.py
+++ main.py
@@ -4,59 +4,7 @@ Created on 12 apr. 2019.
 @author: yulay.gafarov
 '''
 import pygame
-import random
-
-
-class Life:
-    NROWS = 90
-    NCOLS = 90
-
-    def __init__(self):
-        self.ncells = 0
-        self.step = 0
-        self.field = [0] * self.NCOLS
-        for icol in range(self.NCOLS):
-            self.field[icol] = [0] * self.NROWS
-        self.randomizeField()
-            
-    def randomizeField(self):
-        for icol in range(self.NCOLS):
-            for irow in range(self.NROWS):
-                value = random.randint(0, 30)
-                self.field[icol][irow] = 1 if value == 0 else 0
-    
-    def printField(self):
-        for icol in range(self.NCOLS):
-            print(self.field[icol])
-    
-    def count_next_step(self):
-        for icol in range(self.NCOLS):
-            for irow in range(self.NROWS):
-                nneighbours = self._countNeighbours(icol, irow)
-                if self.field[icol][irow] == 0 and nneighbours == 3:
-                    self.field[icol][irow] = 1
-                if self.field[icol][irow] == 1 and (nneighbours < 2 or nneighbours > 3):
-                    self.field[icol][irow] = 0
-            
-    def _countNeighbours(self, col, row):
-        counterNeighbours = 0
-        if self.field[col - 1][row - 1] == 1:
-            counterNeighbours += 1
-        if self.field[col][row - 1] == 1:
-            counterNeighbours += 1
-        if self.field[(col + 1) % (self.NCOLS - 1)][row - 1] == 1:
-            counterNeighbours += 1
-        if self.field[col - 1][row] == 1:
-            counterNeighbours += 1
-        if self.field[(col + 1) % (self.NCOLS - 1)][row] == 1:
-            counterNeighbours += 1
-        if self.field[col - 1][(row + 1) % (self.NROWS - 1)] == 1:
-            counterNeighbours += 1
-        if self.field[col ][(row + 1) % (self.NROWS - 1)] == 1:
-            counterNeighbours += 1
-        if self.field[(col + 1) % (self.NCOLS - 1)][(row + 1) % (self.NROWS - 1)] == 1:
-            counterNeighbours += 1
-        return counterNeighbours
+from lifegame import Lifegame
 
 
 class Game:
@@ -65,23 +13,23 @@ class Game:
     CELL_SIZE = 10
     
     def __init__(self):
-        self.life = Life()
-        self.screen_width = self.life.NCOLS * self.CELL_SIZE
-        self.screen_height = self.life.NROWS * self.CELL_SIZE
+        self.life = Lifegame(60, 60)
+        self.screen_width = self.life.ncols * self.CELL_SIZE
+        self.screen_height = self.life.nrows * self.CELL_SIZE
         pygame.init()
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.end = False
         
     def showState(self):
         self.screen.fill(self.WHITE) 
-        for icol in range(self.life.NCOLS):
-            for irow in range(self.life.NROWS):
+        for icol in range(self.life.ncols):
+            for irow in range(self.life.nrows):
                 if self.life.field[icol][irow] == 1:
                     pygame.draw.rect(self.screen, self.BLACK, (icol * self.CELL_SIZE, irow * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE))
         pygame.display.update()
         
     def nextState(self):
-        self.life.count_next_step()
+        self.life.calculate_next_generation()
     
     def processEvents(self):
         for event in pygame.event.get():
