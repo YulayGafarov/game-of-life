2eea47cb8d091bd253bd9084b22d7a84a90a00c9
Yulay Gafarov <yulay.gafarov@gmail.com>
2019-04-13 00:21:16 +0500

    little refactor with colors

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from lifegame import Lifegame
 class LifeGameVisualizer:
     CELL_COLOR = (69, 139, 0)
     SCREEN_COLOR = (255, 255, 255)
+    BLACK = (0, 0, 0)
     CELL_SIZE = 16
     
     def __init__(self):
@@ -32,9 +33,9 @@ class LifeGameVisualizer:
     def _drawBackground(self):
         self.screen.fill(self.SCREEN_COLOR)
         for icol in range(self.life.ncols):
-            pygame.draw.line(self.screen, (0, 0, 0), (icol * self.CELL_SIZE, 0), (icol * self.CELL_SIZE, self.screen_height))
+            pygame.draw.line(self.screen, self.BLACK, (icol * self.CELL_SIZE, 0), (icol * self.CELL_SIZE, self.screen_height))
         for irow in range(self.life.nrows):
-            pygame.draw.line(self.screen, (0, 0, 0), (0, irow * self.CELL_SIZE), (self.screen_width, irow * self.CELL_SIZE))
+            pygame.draw.line(self.screen, self.BLACK, (0, irow * self.CELL_SIZE), (self.screen_width, irow * self.CELL_SIZE))
     
     def _drawCell(self, icol, irow):
         pygame.draw.circle(self.screen, self.CELL_COLOR, (icol * self.CELL_SIZE + int(self.CELL_SIZE / 2), irow * self.CELL_SIZE + int(self.CELL_SIZE / 2)), int(self.CELL_SIZE / 2))
